93ce3fb49254e50bc35bd6fe36053c04daf220d6
Marwa Ahmadzai <marwa.ah727@gmail.com>
2026-06-24 14:00:23 -0400

    Initial Commit

---
+++ Organize/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Organize
+//
+//  Created by Marwa Ahmadzai on 2026-06-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Organize/OrganizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrganizeApp.swift
+//  Organize
+//
+//  Created by Marwa Ahmadzai on 2026-06-24.
+//
+
+import SwiftUI
+
+@main
+struct OrganizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
